d16e68a7a595f14935a06eceaaa491fc0138d932
stanken_a <artyemst3@mail.ru>
2023-04-12 14:03:07 +0300

    feat: add adaptive version of index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.esm.browser.min.js';
 
+const menuBtn = document.querySelector('.header__menu-btn');
+const menuBottom = document.querySelector('.header__bottom');
+
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('header__menu-btn_opened');
+  menuBottom.classList.toggle('header__bottom_closed');
+});
+
 const heroSwiper = new Swiper('.hero__swiper', {
   grabCursor: true,
   loop: true,
@@ -14,9 +22,26 @@ const heroSwiper = new Swiper('.hero__swiper', {
 });
 
 const recommendSwiper = new Swiper('.recommend__swiper', {
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 1025px
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 21,
+    },
+    1440: {
+      slidesPerView: 4,
+      spaceBetween: 21,
+    }
+  },
   grabCursor: true,
-  slidesPerView: 4,
-  spaceBetween: 21,
   pagination: {
     el: ".recommend__swiper-pagination",
     clickable: true,
@@ -28,9 +53,26 @@ const recommendSwiper = new Swiper('.recommend__swiper', {
 });
 
 const newsSwiper = new Swiper('.news__swiper', {
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 1025px
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1440: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    }
+  },
   grabCursor: true,
-  slidesPerView: 4,
-  spaceBetween: 20,
   pagination: {
     el: ".news__pagination",
     clickable: true,
