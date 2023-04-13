ccd8569775a8d1fbeeb05a579f6b3cd53d638a53
stanken_a <artyemst3@mail.ru>
2023-04-13 19:53:04 +0300

    feat: add popup with form

--- src/pages/pageWithRecommend.js
+++ src/pages/pageWithRecommend.js
@@ -1,7 +1,23 @@
-import { menuBtn, menuBottom } from '../utils/constants.js';
+import { menuBtn, menuBottom, allPopupBtns, closePopup, popup } from '../utils/constants.js';
 import { recommendSwiper } from '../components/swipers.js';
 
 menuBtn.addEventListener('click', () => {
   menuBtn.classList.toggle('header__menu-btn_opened');
   menuBottom.classList.toggle('header__bottom_closed');
+});
+
+allPopupBtns.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    popup.classList.add('popup_opened');
+  })
+});
+
+closePopup.addEventListener('click', () => {
+  popup.classList.remove('popup_opened');
+});
+
+popup.addEventListener('click', (evt) => {
+  if (evt.target.classList.contains('popup_opened')) {
+    popup.classList.remove('popup_opened');
+  }
 });
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,6 @@
 export const menuBtn = document.querySelector('.header__menu-btn');
 export const menuBottom = document.querySelector('.header__bottom');
+
+export const allPopupBtns = document.querySelectorAll('.button_popup');
+export const popup = document.querySelector('.popup');
+export const closePopup = popup.querySelector('.popup__close');
